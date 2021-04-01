d89d59bce7e96169e6ec5c17a618df884f7415a3
Asif10H <habibullahasif71@gmail.com>
2021-04-02 02:54:59 +0600

    added css

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,21 +6,20 @@ const Product = (props) => {
     console.log(name, authorName, addPrice, imageURL, _id);
 
     return (
-       
-            <div className="col-sm-12  col-md-3 col-lg-3 mt-5">
-                <div className="card h-100">
-                    <img src={imageURL} className="card-img-top img-fluid w-100 h-50" alt={name} />
-                    <div className="card-body">
-                        <h5 className="card-title">{name}</h5>
-                        <p className="card-subtitle mb-2 text-muted">{authorName}</p>
-                        <div className="d-flex justify-content-between w-100  ">
-                            <p className="card-text">${addPrice}</p>
-                            <Link to="/" className="card-link btn btn-primary">Buy Now</Link>
-                        </div>
+        <div className="col-lg-4 col-md-6 col-sm-12 m-3 p-3" style={{ width: '18rem' }}>
+            <div className="card h-100">
+                <img src={imageURL} className="card-img-top img-fluid" alt={name} />
+                <div className="card-body">
+                    <h5 className="card-title">{name}</h5>
+                    <p className="card-subtitle mb-2 text-muted">{authorName}</p>
+                    <div className="d-flex justify-content-between">
+                        <p className="card-text">${addPrice}</p>
+                        <Link to="/" className="card-link btn btn-primary">Buy Now</Link>
                     </div>
                 </div>
             </div>
-        
+        </div>
+
 
     );
 };
